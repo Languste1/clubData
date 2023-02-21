562bec4e8c6e6684d70f663a3d4d091b03d354a9
ChristianLange <christian.lange@brightstraining.com>
2023-02-21 11:49:15 +0100

    added more players and checked a specific time where players where playing for other clubs

--- club1.2.sql
+++ club1.2.sql
@@ -49,6 +49,21 @@ WHERE   idSpieler = 4;
 UPDATE spieler SET  verliehen_bis = '2022-04-13' 
 WHERE   idSpieler = 5;
 
+INSERT spieler (name, Trainer_idTrainer, Verein_idVerein, verliehen_seit, verliehen_bis) 
+VALUES ( "Test5", 1, 1, '2018-09-12','2019-06-30');
 
+INSERT spieler (name, Trainer_idTrainer, Verein_idVerein, verliehen_seit, verliehen_bis) 
+VALUES ( "Test6", 1, 1, '2021-01-15','2022-07-08');
+
+INSERT spieler (name, Trainer_idTrainer, Verein_idVerein, verliehen_seit, verliehen_bis) 
+VALUES ( "Test7", 1, 1, '2019-11-20','2022-02-13');
+
+INSERT spieler (name, Trainer_idTrainer, Verein_idVerein, verliehen_seit, verliehen_bis) 
+VALUES ( "Test8", 1, 1, '2021-12-19','2022-08-11');
+
+
+SELECT *
+FROM spieler
+WHERE verliehen_seit > '2020-08-10' AND verliehen_bis < '2022-03-06';
 
 
